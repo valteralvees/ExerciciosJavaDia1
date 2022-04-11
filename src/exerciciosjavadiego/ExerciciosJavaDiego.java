f0828d08a4eeb64033f614783285dd946849e06e
Valter <valteralvees@outlook.com>
2022-04-11 14:56:19 -0300

    exerciciopintura

--- src/exerciciosjavadiego/ExerciciosJavaDiego.java
+++ src/exerciciosjavadiego/ExerciciosJavaDiego.java
@@ -14,8 +14,25 @@ public class ExerciciosJavaDiego {
         System.out.println("Quantos metros você pintará?");
         double metros = teclado.nextDouble();
         //Galão
-        while (metros > 108 ){
-            int qtd_galoes = 1;
+        double rendgalao = metros / QTD_METROS_GALAO;
+        double modgalao = metros % QTD_METROS_GALAO;
+        int totalgaloes =  (int)rendgalao;
+        double valortotalgalao = totalgaloes * 60; 
+        if(modgalao != 0){
+            totalgaloes = totalgaloes +1;
         }
+        System.out.println("Você precisará de " + totalgaloes + " galões para fazer a pintura de "+ metros+ " metros de parede. Você precisará de R$"+valortotalgalao+ " para utilizar essa opção.");
         
+        //Latão
+        double rendlatao = metros / QTD_METROS_LATAO;
+        double modlatao = metros % QTD_METROS_LATAO;
+        int totallatoes =  (int)rendlatao;
+        double valortotallatao = totallatoes * 25;
+        if(modlatao != 0){
+            totallatoes = totallatoes +1;
+        }
+        System.out.println("Você precisará de " + totallatoes + " latões para fazer a pintura de "+metros+ " metros de parede. Você precisará de R$"+valortotallatao+ " para utilizar essa opção.");
+         
+            
+    }        
 }
