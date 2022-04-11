4546617e4f0354f1ec3c93bb13e09bddde329689
Valter <valteralvees@outlook.com>
2022-04-11 10:27:57 -0300

    Initial commit

---
+++ src/exerciciosjavadiego/ExerciciosJavaDiego.java
@@ -0,0 +1,21 @@
+package exerciciosjavadiego;
+import java.util.Scanner;
+public class ExerciciosJavaDiego {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao simulador de pintura IQUINE. Aqui você saberá a quantidade de galões ou latas necessários para fazer sua obra.");
+        
+        final double GALAO = 18;
+        final double QTD_METROS_GALAO = 108;
+        
+        final double LATAO = 3.6;
+        final double QTD_METROS_LATAO = 21.6;
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Quantos metros você pintará?");
+        double metros = teclado.nextDouble();
+        //Galão
+        while (metros > 108 ){
+            int qtd_galoes = 1;
+        }
+        
+}
